353711be0746d5c083ffe313795e58f8faedbf10
Jennamaslovich <108747993+Jennamaslovich@users.noreply.github.com>
2022-08-22 09:23:19 -0400

    textualdata

---
+++ lesson3textualdata3/src/ShoppingCart01.java
@@ -0,0 +1,17 @@
+public class ShoppingCart01 {
+
+        public static void main(String[] args) {
+            String x = "Alex Wants to Purchase a shirt";
+            System.out.println("Alex wants to purchase a shirt");
+
+        
+        
+        
+        
+        
+        
+        
+        // Print and run the code
+        
+    }
+}
